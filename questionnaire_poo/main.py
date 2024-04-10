f5e3d5177c831c4b2d8e6e5ac1a8ab9e7addb3f8
abenrejeb <ext-abenrejeb@leparisien.fr>
2024-04-10 15:13:16 +0200

    [Section-25] Class questionnaire

---
+++ questionnaire_poo/main.py
@@ -0,0 +1,82 @@
+# LES FONCTIONS : PROJET QUESTIONNAIRE
+#
+# Question : Quelle est la capitale de la France ?
+# (a) Marseille
+# (b) Nice
+# (c) Paris
+# (d) Nantes
+#
+# Votre réponse :
+# Bonne réponse / Mauvaise réponse
+
+#
+# Question : Quelle est la capitale de l'Italie ?
+#
+#
+# 4 questions
+
+# Question
+#     - titre         - str
+#     - choix         - liste de str
+#     - bonne_reponse - str
+#
+#     - poser()       - bool
+#
+# Questionnaire
+#     - questions      - liste de Question
+#
+#     - lancer()       - void
+#
+
+class Question:
+    def __init__(self, titre, choix, bonne_reponse):
+        self.titre = titre
+        self.choix = choix
+        self.bonne_reponse = bonne_reponse
+
+    def poser(self):
+        print("QUESTION")
+        print("  " + self.titre)
+        for i in range(len(self.choix)):
+            print("  ", i+1, "-", self.choix[i])
+
+        print()
+        resultat_response_correcte = False
+        reponse_int = Question.demander_reponse_numerique_utlisateur(1, len(self.choix))
+        if self.choix[reponse_int-1].lower() == self.bonne_reponse.lower():
+            print("Bonne réponse")
+            resultat_response_correcte = True
+        else:
+            print("Mauvaise réponse")
+
+        print()
+        return resultat_response_correcte
+
+    def demander_reponse_numerique_utlisateur(min, max):
+        reponse_str = input("Votre réponse (entre " + str(min) + " et " + str(max) + ") : ")
+        try:
+            reponse_int = int(reponse_str)
+            if min <= reponse_int <= max:
+                return reponse_int
+
+            print("ERREUR : Vous devez rentrer un nombre entre", min, "et", max)
+        except:
+            print("ERREUR : Veuillez rentrer uniquement des chiffres")
+        return Question.demander_reponse_numerique_utlisateur(min, max)
+
+class Questionnaire:
+    def __init__(self, questions):
+        self.questions = questions
+
+    def lancer(self):
+        score = 0
+        for question in self.questions:
+            if question.poser():
+                score += 1
+        print("Score final :", score, "sur", len(self.questions))
+
+Questionnaire((
+    Question("Quelle est la capitale de la France ?", ("Marseille", "Nice", "Paris", "Nantes", "Lille"), "Paris"), 
+    Question("Quelle est la capitale de l'Italie ?", ("Rome", "Venise", "Pise", "Florence"), "Rome"),
+    Question("Quelle est la capitale de la Belgique ?", ("Anvers", "Bruxelles", "Bruges", "Liège"), "Bruxelles")
+)).lancer()
